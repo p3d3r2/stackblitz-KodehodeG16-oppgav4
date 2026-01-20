0b83436d591f6da6ddad26aa19787bab2fa43370
p3d3r2 <kleppepeder@gmail.com>
2026-01-20 14:35:28 +0100

    js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 console.log('Hello!');
 
-const todoButton = 
+const todoButton = document.querySelector("#todoButton");
+
+todoButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log('Dette er en gjøremålsliste med følgende innhold:');
+
+    const inputTask = document.querySelector("#inputTask").value;
+    console.log(inputTask)
+
+    const taskList = document.querySelector("#taskList")
+    const taskItem = document.createElement("li")
+    taskItem.textContent = inputTask
+    taskList.appendChild(taskItem)
+
+    
+})
