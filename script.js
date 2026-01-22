124c6764f5adde97567b62794a8073130acf0fee
p3d3r2 <kleppepeder@gmail.com>
2026-01-22 13:34:45 +0100

    js ferdig

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('Hello!');
 
+/*
 const todoButton = document.querySelector("#todoButton");
 
 todoButton.addEventListener("click", function (event) {
@@ -16,3 +17,47 @@ todoButton.addEventListener("click", function (event) {
 
     
 })
+
+*/
+
+const inputTask = document.querySelector("#inputTask");
+const taskList = document.querySelector("#taskList");
+const todoButton = document.querySelector("#todoButton");
+
+todoButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    
+    if (!inputTask.value == "") {
+        const inputTaskValue = inputTask.value;
+        
+    const todoItem = document.createElement("li");
+    todoItem.textContent = inputTaskValue;
+    taskList.appendChild(todoItem)
+
+    const finishButton = document.createElement("Button")
+    finishButton.textContent = "Ferdig"
+    finishButton.addEventListener("click", function () {
+        todoItem.style.textDecoration = "line-through"
+
+    })
+
+    todoItem.appendChild(finishButton)
+
+    const deleteButton = document.createElement("Button")
+    deleteButton.textContent = "Slett"
+    deleteButton.addEventListener("click", function () {
+        todoItem.remove()
+
+    })
+
+    todoItem.appendChild(deleteButton)
+
+    inputTask.value = "";
+    } else {
+        console.log("noe gikk galt")
+    }
+
+
+    
+});
+
